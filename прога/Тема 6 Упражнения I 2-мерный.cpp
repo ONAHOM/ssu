07a2f9a5534a1f245804efce342784cfd3bdedec
onahom <typ.fu222@gmail.com>
2024-11-19 15:56:09 +0400

    курлык

---
+++ прога/Тема 6 Упражнения I 2-мерный.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n,m,x1,x2,summ = 0,c=1;
+    cout << "Введите коллиство строк и столбцов двумерного массива (1 <= n,m <= 9)\n";
+    cin >> n >> m;
+    cout << "Введите промжуток суммы\n";
+    cin >> x1 >> x2;
+    int mass[10][10];
+    for (int i = 0; i < n; ++i){
+        for (int j = 0; j < m; ++j){
+            cout << "mass[" << i << "][" << j << "] = ";
+            cin >> mass[i][j];
+        }
+    }
+    for (int i = 0; i < n; ++i){
+        for (int j = 0; j < m; ++j){
+            if (c <= x2 and c >= x1){
+                summ += mass[i][j];
+            }
+            c++;
+        }
+    }
+    cout << summ;
+    return 0;
+}
+    
